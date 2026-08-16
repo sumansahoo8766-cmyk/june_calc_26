a7cf56cddf1741d20f74fab3cea4100fe9b92999
suman sahoo <sumansahoo8766@gmail.com>
2026-08-16 18:49:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,65 @@
 import streamlit as st
 
-# Title
-st.title("💰 Simple Interest Calculator")
+st.title("🧮 Simple Calculator")
 
-st.write("This application calculates Simple Interest and Maturity Amount.")
-
-# User Inputs
-principle = st.number_input(
-    "Enter the Principal Amount (₹)",
-    min_value=0.0,
-    value=1000.0,
-    step=100.0
+operator = st.selectbox(
+    "Choose an operation:",
+    ["Addition (+)", "Subtraction (-)", "Multiplication (*)", "Division (/)" ]
 )
 
-rate = st.number_input(
-    "Enter Rate of Interest (%)",
-    min_value=0.0,
-    value=5.0,
-    step=0.1
-)
+if operator == "Addition (+)":
+    values = st.text_input(
+        "Enter values separated by comma:",
+        placeholder="10,20,30,40"
+    )
 
-year = st.number_input(
-    "Enter Time (Years)",
-    min_value=0,
-    value=1,
-    step=1
-)
+    if st.button("Calculate"):
+        try:
+            new_list = [float(val.strip()) for val in values.split(",")]
+            result = sum(new_list)
+            st.success(f"Sum of all given values = {result}")
+        except ValueError:
+            st.error("Please enter valid numbers separated by commas.")
 
-month = st.number_input(
-    "Enter Additional Months",
-    min_value=0,
-    max_value=11,
-    value=0,
-    step=1
-)
 
-# Calculate Button
-if st.button("Calculate"):
-    time = year + (month / 12)
-    interest = (principle * rate * time) / 100
-    maturity_amount = principle + interest
+elif operator == "Subtraction (-)":
+    num1 = st.number_input("Enter first value:", value=0.0)
+    num2 = st.number_input("Enter second value:", value=0.0)
 
-    st.success(f"✅ Simple Interest: ₹{interest:.2f}")
-    st.info(f"💵 Maturity Amount: ₹{maturity_amount:.2f}")
+    if st.button("Calculate"):
+        st.success(f"{num1} - {num2} = {num1 - num2}")
+        st.success(f"{num2} - {num1} = {num2 - num1}")
+
+
+elif operator == "Multiplication (*)":
+    values = st.text_input(
+        "Enter values separated by comma:",
+        placeholder="2,3,4"
+    )
+
+    if st.button("Calculate"):
+        try:
+            new_list = [float(val.strip()) for val in values.split(",")]
+
+            result = 1
+            for val in new_list:
+                result = result * val
+
+            st.success(f"Multiplication of all values = {result}")
+
+        except ValueError:
+            st.error("Please enter valid numbers separated by commas.")
+
+
+elif operator == "Division (/)":
+    num1 = st.number_input("Enter first value:", value=0.0)
+    num2 = st.number_input("Enter second value:", value=0.0)
+
+    if st.button("Calculate"):
+        if num2 != 0:
+            st.success(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            st.error("You cannot divide by zero.")
+
+st.write("---")
+st.write("Thank you! 😊")
